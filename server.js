08d4c403001d26b5bf3fb7170e48ef76b68211f6
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-22 17:34:39 +0330

    feat: enhance server shutdown handling with SIGTERM and SIGINT support

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
+
 import { connectToDB } from "./require/configDB.js";
 
 async function bootstrap() {
   await connectToDB();
+
   const port = process.env.PORT || 3000;
 
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`Server started on ${port}`);
+  });
+
+  process.on("SIGTERM", () => {
+    console.log("SIGTERM received. Closing server...");
+
+    server.close(() => {
+      console.log("HTTP server closed.");
+      process.exit(0);
+    });
+  });
+
+  process.on("SIGINT", () => {
+    console.log("SIGINT received. Closing server...");
+
+    server.close(() => {
+      console.log("HTTP server closed.");
+      process.exit(0);
+    });
   });
 }
 
