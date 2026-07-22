5dc31707521aa70d6c84f6e5720763412645f033
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-22 17:34:19 +0330

    feat: add CORS and rate limiting middleware for improved security and performance

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 const app = express();
+import cors from "cors";
 import router from "./routes.js";
 import helmet from "helmet";
+import errorHandler from "./require/errorHandler.js";
+import rateLimit from "express-rate-limit";
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+  }),
+);
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 200,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+      success: false,
+      message: "Too many requests, please try again later.",
+    },
+  }),
+);
 
 app.use(cookieParser());
 app.use(express.json());
@@ -10,4 +33,5 @@ app.use(helmet());
 
 app.use("/api", router);
 
+app.use(errorHandler);
 export default app;
