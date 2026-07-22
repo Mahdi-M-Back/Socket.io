f4b49eb0ce3251e30aafa72c22007ae03d107005
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-22 17:34:31 +0330

    feat: add health check endpoint and update routes
    - Added a health check endpoint at /health that returns a success status.
    - Updated user and chat routes to /users and /chats respectively.
    - Enhanced package.json with new scripts for database migrations and added dependencies for CORS, rate limiting, and logging.

--- routes.js
+++ routes.js
@@ -4,7 +4,15 @@ const router = Router();
 import userRouter from "./module/user/router.js";
 import chatRouter from "./module/chat/router.js";
 
-router.use("/user", userRouter);
-router.use("/chat", chatRouter);
+router.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    status: "OK",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+router.use("/users", userRouter);
+router.use("/chats", chatRouter);
 
 export default router;
