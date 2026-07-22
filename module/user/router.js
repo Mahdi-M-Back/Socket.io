2a24f6ce448a03339953db7fcae0959fa29568ac
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-22 16:20:01 +0330

    feat: implement user authentication routes and middleware

--- module/user/router.js
+++ module/user/router.js
@@ -1,4 +1,21 @@
 import { Router } from "express";
-const router = Router()
+import * as controller from "./controller.js";
+import verify from "./../../require/jwt.js";
+
+const router = Router();
+
+router.post("/signup", controller.signup);
+router.post("/login", controller.login);
+router.post("/refresh-token", controller.refreshToken);
+
+router.use(verify.protect);
+
+router
+  .route("/me")
+  .patch(controller.updateMe)
+  .get(controller.getMe)
+  .delete(controller.deleteMe);
+
+router.post("/logout", controller.logout);
 
 export default router;
